96130b4e0cf81288c40a130ad8de624b4916389f
Sheila Wittman <szandora789@gmail.com>
2018-05-30 22:23:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,14 +1,7 @@
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+ 
+# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -18,81 +11,79 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input.to_i - 1
+  (1..9).include?(user_input.to_i) ? user_input.to_i - 1 : -1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
+def move(board, pos, bleh)
+  board[pos] = bleh
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(board,index)
+  position_taken?(board,index) || !(0..8).include?(index) ? false : true
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player)
+  input = gets.chomp
+  pos = input_to_index(input)
+  validation = valid_move?(board,pos)
+  if validation
+    # puts "Please enter 1-9:"
+    # input = gets.chomp
+    # pos = input_to_index(input)
+    # validation = valid_move?(board,pos)
+    move(board,pos,current_player(board))
     display_board(board)
-  else until valid_move?(board, index)
-    puts "Sorry, that was an invalid move. Please enter 1-9:"
+  else
     turn(board)
   end
 end
 
 def turn_count(board)
-  board.count { |x| x == "X" || x == "O"}
+  count = 0
+  board.each{|elm| count += 1 if ["X","O"].include?(elm)}
+  count
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    "X"
-  else
-    "O"
-  end
+  (turn_count(board)+1) % 2 == 0 ? "O" : "X"
 end
 
 def won?(board)
-  WIN_COMBINATIONS.find do |win|
-    board[win[0]] == board[win[1]] && board[win[0]] == board[win[2]] && position_taken?(board, win[1])
+  WIN_COMBINATIONS.find do |combo|
+    (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") || (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
   end
 end
 
 def full?(board)
-  board.none?{|i| i == " " || i.nil?}
+  !board.include?(" ")
 end
 
 def draw?(board)
-  if full?(board) && !won?(board)
-    true
-  end
+  !won?(board) && full?(board)
 end
 
 def over?(board)
-  if won?(board) || full?(board) || draw?(board)
-    true
-  end
+  won?(board) || draw?(board) || full?(board)
 end
 
 def winner(board)
-  if won?(board)
-    winner = board[won?(board)[0]]
-  end
+  won?(board) ? board[won?(board)[0]] : nil
 end
 
 def play(board)
-  until over?(board) == true
+  until over?(board)
     turn(board)
   end
-  if draw?(board) == true
-    "Cats Game!"
-  else won?(board)
-    "Congratulations #{winner(board)}"
+
+  # won?(board) ? "Congratulations #{winner(board)}!" : "Cat's Game!"
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else
+    puts "Cat's Game!"
   end
 end
